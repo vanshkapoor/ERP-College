706044af2e0d66a1d8eb77623f767c6daff17ea0
Priya Goel <priyagoel99@gmail.com>
2020-05-31 00:37:37 +0530

    added and updated

---
+++ tr.js
@@ -0,0 +1,37 @@
+function tprofile()
+{
+	document.getElementById("1").style.display = "block";
+	
+
+        document.getElementById("2").style.display = "none";
+		document.getElementById("3").style.display = "none";
+		document.getElementById("4").style.display = "none";
+}
+function tattendence()
+{
+	document.getElementById("1").style.display = "none";
+	
+
+        document.getElementById("2").style.display = "inline-block";
+		document.getElementById("3").style.display = "none";
+		document.getElementById("4").style.display = "none";
+}
+function tassignment()
+{
+	document.getElementById("1").style.display = "none";
+	
+
+        document.getElementById("2").style.display = "none";
+		document.getElementById("3").style.display = "block";
+		document.getElementById("4").style.display = "none";
+}
+
+function tresult()
+{
+	document.getElementById("1").style.display = "none";
+	
+
+        document.getElementById("2").style.display = "none";
+		document.getElementById("3").style.display = "none";
+		document.getElementById("4").style.display = "block";
+}
