61a1d75d1349d9c9198f3978274c428c78027d72
MuskanSr <muskan.srivastava1904@gmail.com>
2020-06-05 16:50:23 +0530

    Removed files

--- helpers/auth.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    ensureAuthenticated: function(req, res, next) {
-      if (req.isAuthenticated()) { // because of passport
-        return next();
-      }
-      req.flash('error_msg', 'Not authorized');
-      res.redirect('//login');// when handel bars added give the path here muskan bhumika
-    }
-  }
